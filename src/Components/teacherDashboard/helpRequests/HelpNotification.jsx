6d3f01c4d8a1c03b2c18c4d7c3341ba835deee71
Toby Langdon <tobyjoelangdon@gmail.com>
2023-09-17 23:41:39 +1200

    Installed font awesome, not sure if we actually need it. Most of help requests is complete.

---
+++ src/Components/teacherDashboard/helpRequests/HelpNotification.jsx
@@ -0,0 +1,5 @@
+import styles from "./HelpNotification.module.css";
+
+export default function HelpNotification({ name, gender, icon, datetime }) {
+    return <div className={styles.main}>Test</div>;
+}
--- src/Components/teacherDashboard/helpRequests/HelpRequests.jsx
+++ src/Components/teacherDashboard/helpRequests/HelpRequests.jsx
@@ -1,4 +1,5 @@
 import styles from "./HelpRequests.module.css";
+import HelpNotification from "./HelpNotification";
 
 export default function HelpRequests() {
     return (
@@ -6,9 +7,17 @@ export default function HelpRequests() {
             <div className={styles.helpWrapper}>
                 <div className={styles.helpMainContent}>
                     <h3>HELP REQUESTS</h3>
-                    <div className={styles.actionButtons}>
-                        <button>REPLY</button>
-                        <button>MARK AS DONE</button>
+                    <div className={styles.notificationWrapper}>
+                        <div className={styles.actionButtons}>
+                            <button>
+                                <i class="fa-solid fa-reply"></i>
+                                REPLY
+                            </button>
+                            <button>MARK AS DONE</button>
+                        </div>
+                        <div className={styles.notifications}>
+                            <HelpNotification />
+                        </div>
                     </div>
                 </div>
             </div>
