ad8c6a15b6ed8338e78ce44f8c5269f3efb4920b
Neihana-G <neihana@missionreadyhq.com>
2023-09-18 19:55:56 +1200

    Add progress tracker files

---
+++ src/common/teacherDashboard/progressTracker/ProgressTracker.jsx
@@ -0,0 +1,7 @@
+import "./ProgressTracker.css";
+
+export default function ProgressTracker() {
+  return <div id="speed">SPEED</div>;
+}
+// // note for opening you are setting up the slots for the features of the page
+//
---
+++ src/pages/Home/TeacherDashboard/TeacherDashboard.jsx
@@ -0,0 +1,20 @@
+import ProgressTracker from "../../Components/common/teacherDashboard/progressTracker/ProgressTracker";
+import "./TeacherDashboard.css";
+
+export default function TeacherDashboard() {
+  return (
+    <div id="background-wrapper">
+      <div id="header-bar">HEADER BAR</div>
+      <div id="sidebar" className="grid-item narrow">
+        SIDE BAR
+      </div>
+      <div id="tracker-border" className="grid-item wide">
+        <div id="tracker-container" className="grid-item">
+          <ProgressTracker />
+        </div>
+        tracker border
+      </div>
+      <div id="footer-bar">FOOTER BAR </div>
+    </div>
+  );
+}
