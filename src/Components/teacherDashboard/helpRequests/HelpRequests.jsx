071f0fd5f369f88899a115c73a826547f54121ec
Toby Langdon <tobyjoelangdon@gmail.com>
2023-09-19 00:56:44 +1200

    Completed scroll bar for help requests page

--- src/Components/teacherDashboard/helpRequests/HelpRequests.jsx
+++ src/Components/teacherDashboard/helpRequests/HelpRequests.jsx
@@ -1,40 +1,99 @@
 import styles from "./HelpRequests.module.css";
 import HelpNotification from "./HelpNotification";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 
 export default function HelpRequests() {
     const [contentHeight, setContentHeight] = useState(0);
     const [scrollHeight, setScrollHeight] = useState(0);
     const [currentScrollPosition, setCurrentScrollPosition] = useState(0);
     const [thumbPos, setThumbPos] = useState(0);
-    const [thumbHeight, setThumbHeight] = useState(20);
+    const [thumbHeight, setThumbHeight] = useState(0);
     const content = useRef(null);
     const viewport = useRef(null);
     const scrollbar = useRef(null);
     const scrollbar_thumb = useRef(null);
     const [scrollPosition, setScrollPosition] = useState(0);
-    const [scrollBarVisible, setScollBarVisible] = useState(false);
+    const [scrollBarVisible, setScollBarVisible] = useState(true);
+
+    // Draggin scrollbar thumb
+    const [isDragging, setIsDragging] = useState(false);
+    const [initialScrollTop, setInitialScrollTop] = useState(0);
+    const [initialThumbPos, setInitialThumbPos] = useState(0);
+    const [scrollStartPos, setScrollStartpos] = useState(0);
+
+    const handleMouseDown = useCallback((e) => {
+        e.preventDefault();
+        // e.stopPropagation();
+        setScrollStartpos(e.clientY);
+        console.log("thumb: ", thumbPos);
+        if (content.current) setInitialScrollTop(content.current.scrollTop);
+        console.log("Mouse down");
+        setIsDragging(true);
+    }, []);
+
+    const handleMouseUp = useCallback(
+        (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            if (isDragging) {
+                setIsDragging(false);
+            }
+        },
+        [isDragging]
+    );
+
+    const handleThumbMousemove = useCallback((e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (isDragging) {
+            console.log(scrollStartPos, e.clientY);
+            const deltaY = scrollStartPos - e.clientY;
+            content.current.scrollTop = initialScrollTop - deltaY * 2;
+        }
+    });
+    useEffect(() => {
+        document.addEventListener("mousemove", handleThumbMousemove);
+        document.addEventListener("mouseup", handleMouseUp);
+        document.addEventListener("mouseleave", handleMouseUp);
+        return () => {
+            document.removeEventListener("mousemove", handleThumbMousemove);
+            document.removeEventListener("mouseup", handleMouseUp);
+            document.removeEventListener("mouseleave", handleMouseUp);
+        };
+    }, [handleThumbMousemove, handleMouseUp]);
 
     const handleScroll = () => {
-        const scrollHeight =
-            content.current.scrollHeight - content.current.clientHeight;
-        setScrollPosition(content.current.scrollTop / scrollHeight);
+        if (scrollbar.current && content.current) {
+            const scrollHeight =
+                content.current.scrollHeight - content.current.clientHeight;
+            setScrollPosition(content.current.scrollTop / scrollHeight);
+        }
     };
+    useEffect(() => {
+        if (scrollbar.current && content.current && thumbHeight == 0) {
+            console.log(content.current.clientHeight);
+            setThumbHeight(
+                Math.round(
+                    (scrollbar.current.clientHeight /
+                        content.current.scrollHeight) *
+                        600
+                )
+            );
+            if (content.current.clientHeight >= content.current.scrollHeight) {
+                setScollBarVisible(false);
+            }
+        }
+    }, [content]);
 
     useEffect(() => {
-        setThumbPos(
-            scrollbar.current.clientHeight * scrollPosition -
-                thumbHeight * scrollPosition
-        );
-
-        setThumbHeight(
-            Math.round(
-                (scrollbar.current.clientHeight /
-                    content.current.scrollHeight) *
-                    400
-            )
-        );
-    });
+        if (scrollbar.current && content.current) {
+            console.log("use effect");
+            setThumbPos(
+                scrollbar.current.clientHeight * scrollPosition -
+                    thumbHeight * scrollPosition
+            );
+        }
+    }, [scrollPosition]);
 
     return (
         <div className={styles.helpBody}>
@@ -70,20 +129,23 @@ export default function HelpRequests() {
                             <HelpNotification />
                         </div>
                     </div>
-
-                    <div className={styles.scrollbar_wrapper}>
-                        <div className={styles.scrollbar} ref={scrollbar}>
-                            <div className={styles.scrollbar_track}></div>
-                            <div
-                                className={styles.scrollbar_thumb}
-                                ref={scrollbar_thumb}
-                                style={{
-                                    height: `${thumbHeight}px`,
-                                    top: `${thumbPos}px`,
-                                }}
-                            ></div>
+                    {scrollBarVisible ? (
+                        <div className={styles.scrollbar_wrapper}>
+                            <div className={styles.scrollbar} ref={scrollbar}>
+                                <div className={styles.scrollbar_track}></div>
+                                <div
+                                    onMouseDown={handleMouseDown}
+                                    onTouchStart={handleMouseDown}
+                                    className={styles.scrollbar_thumb}
+                                    ref={scrollbar_thumb}
+                                    style={{
+                                        height: `${thumbHeight}px`,
+                                        top: `${thumbPos}px`,
+                                    }}
+                                ></div>
+                            </div>
                         </div>
-                    </div>
+                    ) : null}
                 </div>
             </div>
         </div>
