b03da5bc847da28c96396a277c29b6466b408a5f
Toby Langdon <tobyjoelangdon@gmail.com>
2023-09-18 14:37:14 +1200

    Help requests finished

--- src/Components/teacherDashboard/helpRequests/HelpNotification.jsx
+++ src/Components/teacherDashboard/helpRequests/HelpNotification.jsx
@@ -1,5 +1,17 @@
 import styles from "./HelpNotification.module.css";
 
 export default function HelpNotification({ name, gender, icon, datetime }) {
-    return <div className={styles.main}>Test</div>;
+    return (
+        <div className={styles.main}>
+            <input type="checkbox" className={styles.checkbox}></input>
+            <div className={styles.notificationBody}>
+                <img src="/images/students/AidenAndrews.png" />
+                <p>Andrew needs help with his project.</p>
+                <div className={styles.timestamp}>
+                    <p>TUE 28 April 2023</p>
+                    <p>10:43am</p>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/Components/teacherDashboard/helpRequests/HelpRequests.jsx
+++ src/Components/teacherDashboard/helpRequests/HelpRequests.jsx
@@ -1,13 +1,59 @@
 import styles from "./HelpRequests.module.css";
 import HelpNotification from "./HelpNotification";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function HelpRequests() {
+    const [contentHeight, setContentHeight] = useState(0);
+    const [scrollHeight, setScrollHeight] = useState(0);
+    const [currentScrollPosition, setCurrentScrollPosition] = useState(0);
+    const [thumbPos, setThumbPos] = useState(0);
+    const [thumbHeight, setThumbHeight] = useState(20);
+    const content = useRef(null);
+    const viewport = useRef(null);
+    const scrollbar = useRef(null);
+    const scrollbar_thumb = useRef(null);
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const [scrollBarVisible, setScollBarVisible] = useState(false);
+
+    const handleScroll = () => {
+        const scrollHeight =
+            content.current.scrollHeight - content.current.clientHeight;
+        setScrollPosition(content.current.scrollTop / scrollHeight);
+    };
+
+    useEffect(() => {
+        if (content) {
+            if (content.current.scrollHeight > content.current.clientHeight) {
+                console.log(content.current.scrollHeight);
+                console.log(content.current.clientHeight);
+                setScollBarVisible(true);
+            }
+        }
+        console.log(scrollBarVisible);
+        // console.log(content.current.clientHeight);
+        // console.log(content.current.scrollHeight);
+        setThumbPos(
+            scrollbar.current.clientHeight * scrollPosition -
+                thumbHeight * scrollPosition
+        );
+
+        setThumbHeight(
+            Math.round(
+                (scrollbar.current.clientHeight /
+                    content.current.scrollHeight) *
+                    400
+            )
+        );
+    });
+
     return (
         <div className={styles.helpBody}>
             <div className={styles.helpWrapper}>
                 <div className={styles.helpMainContent}>
                     <h3>HELP REQUESTS</h3>
-                    <div className={styles.notificationWrapper}>
+                    {/* Custom scroll bar */}
+
+                    <div className={styles.notificationWrapper} ref={viewport}>
                         <div className={styles.actionButtons}>
                             <button>
                                 <i class="fa-solid fa-reply"></i>
@@ -15,8 +61,37 @@ export default function HelpRequests() {
                             </button>
                             <button>MARK AS DONE</button>
                         </div>
-                        <div className={styles.notifications}>
+
+                        <div
+                            className={styles.notifications}
+                            ref={content}
+                            onScroll={handleScroll}
+                        >
                             <HelpNotification />
+                            <HelpNotification />
+                            {/* <HelpNotification /> */}
+                            {/* <HelpNotification /> */}
+                            {/* <HelpNotification />
+                            <HelpNotification />
+                            <HelpNotification />
+                            <HelpNotification />
+                            <HelpNotification />
+                            <HelpNotification />
+                            <HelpNotification /> */}
+                        </div>
+                    </div>
+
+                    <div className={styles.scrollbar_wrapper}>
+                        <div className={styles.scrollbar} ref={scrollbar}>
+                            <div className={styles.scrollbar_track}></div>
+                            <div
+                                className={styles.scrollbar_thumb}
+                                ref={scrollbar_thumb}
+                                style={{
+                                    height: `${thumbHeight}px`,
+                                    top: `${thumbPos}px`,
+                                }}
+                            ></div>
                         </div>
                     </div>
                 </div>
