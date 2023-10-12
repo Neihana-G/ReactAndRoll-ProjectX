e8200f2d216b1b9e677ad83d92ccf0d314bb692c
Neihana-G <neihana@missionreadyhq.com>
2023-10-12 22:40:30 +1300

    student cards linked with backend

--- src/Components/teacherDashboard/studentProfiles/studentCards/StudentCards.jsx
+++ src/Components/teacherDashboard/studentProfiles/studentCards/StudentCards.jsx
@@ -1,19 +1,36 @@
+import { useEffect, useState } from "react";
 import styles from "./StudentCards.module.css";
 
 const StudentProfiles = () => {
+  const [users, setUsers] = useState();
+
+  useEffect(function () {
+    fetch("http://localhost:4000/api/users")
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setUsers(response);
+      });
+  }, []);
+
   return (
     <div className={styles.studentProfileWrapper}>
-      {/* <h1>Student Profiles</h1> */}
-      {/* <ScrollableContent> */}
-      <div className={styles.studentCard}>
-        <img
-          className={styles.studentPhoto}
-          src="/images/students/AidenAndrews.png"
-          alt="Student 1"
-        />
-        <h3>Student 1</h3>
-        <p>Major: Computer Science</p>
-      </div>
+      {users &&
+        users.map(function (user) {
+          return (
+            <div className={styles.studentCard}>
+              <img
+                className={styles.studentPhoto}
+                src={user.images}
+                alt="Student 1"
+              />
+              <h3>{user.name}</h3>
+              <p>Major: Computer Science</p>
+            </div>
+          );
+        })}
+
+      {/*       
 
       <div className={styles.studentCard}>
         <img
@@ -33,7 +50,7 @@ const StudentProfiles = () => {
         />
         <h3>Student 2</h3>
         <p>Major: Biology</p>
-      </div>
+      </div> */}
     </div>
   );
 };
